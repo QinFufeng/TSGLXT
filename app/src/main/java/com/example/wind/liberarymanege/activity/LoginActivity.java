0198c43d19d66ce5bfabc0aa102458c73d007d88
QinFufeng <1870205385@qq.com>
2017-12-21 17:49:24 +0800

    登陆测试

--- app/src/main/java/com/example/wind/liberarymanege/activity/LoginActivity.java
+++ app/src/main/java/com/example/wind/liberarymanege/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.wind.liberarymanege.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,8 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.wind.liberarymanege.MainActivity;
 import com.example.wind.liberarymanege.R;
-import com.example.wind.liberarymanege.httpdb.HttpConnSoap;
+import com.example.wind.liberarymanege.httpdb.DBUtil;
 
 /**
  * Created by wind on 2017/12/20.
@@ -17,8 +19,8 @@ import com.example.wind.liberarymanege.httpdb.HttpConnSoap;
 public class LoginActivity extends AppCompatActivity{
     private EditText username,password;
     private Button register,login;
-    private HttpConnSoap Test;
-    String cc="test";
+    private DBUtil dbUtil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -28,8 +30,7 @@ public class LoginActivity extends AppCompatActivity{
         password= (EditText) findViewById(R.id.userPassword);
         register= (Button) findViewById(R.id.bn_register);
         login= (Button) findViewById(R.id.bn_login);
-        Test=new HttpConnSoap();
-
+        dbUtil=new DBUtil();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,10 +38,22 @@ public class LoginActivity extends AppCompatActivity{
                 String pwd=password.getText().toString();
                 String [] a=new String[]{"username","userpassword"};
                 String [] b=new String[]{name,pwd};
-                cc=Test.HttpGo(a,b,"IsMatchUser");
-                Toast.makeText(LoginActivity.this, cc, Toast.LENGTH_SHORT).show();
+                int cc=dbUtil.Login(a,b);
+                //Toast.makeText(LoginActivity.this, cc, Toast.LENGTH_SHORT).show();
+                if(cc==0){
+                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent) ;
+                }
+                else if(cc==1){
+                    Toast.makeText(LoginActivity.this,"用户名或密码错误！", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(LoginActivity.this,"网络连接错误！", Toast.LENGTH_SHORT).show();
 
             }
         });
+    }
+
+    public void reg(View view) {
     }
 }
--- app/src/main/java/com/example/wind/liberarymanege/httpdb/DBUtil.java
+++ app/src/main/java/com/example/wind/liberarymanege/httpdb/DBUtil.java
@@ -5,4 +5,21 @@ package com.example.wind.liberarymanege.httpdb;
  */
 
 public class DBUtil {
+    private HttpConnSoap httpConnSoap;
+    //登陆验证
+    public int Login(String[] par1,String[] par2){
+        //boolean cc=false;
+        int c=2;
+        httpConnSoap=new HttpConnSoap();
+        String es=httpConnSoap.HttpGo(par1,par2,"IsMatchUser");
+        if(es.equals("true"))
+        {
+            //Toast.makeText(LoginActivity.class, "用户名或密码错误！", Toast.LENGTH_SHORT).show();
+            c=0;
+        }else if(es.equals("false")){
+            c=1;
+        }
+        //cc=Boolean.parseBoolean(es);
+        return c;
+    }
 }
