94925aac873d6cb4344673caba9e19baa02741bc
QinFufeng <1870205385@qq.com>
2018-01-16 20:31:05 +0800

    用户全部信息获取改进

--- app/src/main/java/com/example/wind/liberarymanege/activity/UpdateUserActivity.java
+++ app/src/main/java/com/example/wind/liberarymanege/activity/UpdateUserActivity.java
@@ -1,5 +1,6 @@
 package com.example.wind.liberarymanege.activity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,8 +22,9 @@ import java.util.List;
 public class UpdateUserActivity extends AppCompatActivity {
     private ImageView image;
     private EditText name,phone,email;
-    private RadioButton man;
-
+    private RadioButton man,woman;
+    private DBUtil dbUtil;
+    private String NAME;
     //private HttpConnSoap2 httpConnSoap2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +35,16 @@ public class UpdateUserActivity extends AppCompatActivity {
         phone= (EditText) findViewById(R.id.updateuserPhone);
         email= (EditText) findViewById(R.id.updauserEmail);
         man= (RadioButton) findViewById(R.id.updaterb_man);
-
+        woman= (RadioButton) findViewById(R.id.updaterb_wnam);
+        Intent in=getIntent();
+        NAME=in.getStringExtra("name");
         /*tt1=(TextView) findViewById(R.id.t1);
         tt2=(TextView) findViewById(R.id.t2);*/
         String[] a=new String[]{"username"};
-        String[] b=new String[]{"aa"};
+        String[] b=new String[]{NAME};
 
         //httpConnSoap2=new HttpConnSoap2();
-        DBUtil dbUtil=new DBUtil();
+        dbUtil=new DBUtil();
         List<TUser> S=dbUtil.ShowUser(a,b);
         TUser tUser=S.get(0);
         TUser ts=S.get(1);
@@ -49,8 +53,10 @@ public class UpdateUserActivity extends AppCompatActivity {
         name.setText(tUser.getUsername());
         if(tUser.getSex().equals("男")){
             man.setChecked(true);
+            woman.setChecked(false);
         }else {
             man.setChecked(false);
+            woman.setChecked(true);
         }
         phone.setText(tUser.getPhone());
         email.setText(tUser.getEmail());
